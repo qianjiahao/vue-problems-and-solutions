b225fbe75e257af61a68d13faa71bd8a27bacf5b
qianjiahao <1041897388@qq.com>
2016-06-09 00:08:15 +0800

    search add shake

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import router from './routers/route.js'
 import App from './views/App.vue'
+import './iconfont/iconfont.css'
 
 router.start(App, '#root')
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -4,13 +4,22 @@
       <div class="logo">
 
       </div>
-      <div class="search">
-        <div class="input">
-          <input class="input-bar" type="text" name="name" v-model="value">
-          <div class="input-clean" @click="clean"></div>
+      <div class="content">
+        <div class="bar">
+          <input
+            class="input"
+            type="text"
+            name="name"
+            v-model="value"
+            @keyup.enter="submit"
+            @click="selectAll"
+            placeholder="空格分隔关键字"
+          >
         </div>
-
-        <div class="submit" @click="submit">搜索</div>
+        <i class="iconfont icon-search search" @click="submit" ></i>
+      </div>
+      <div class="auto" v-show="!!value">
+        sdfsdf
       </div>
     </div>
   </div>
@@ -18,14 +27,30 @@
 
 <script>
 export default {
+  ready () {
+    let speed = 25
+
+    window.addEventListener('devicemotion', (e) => {
+      var acceleration = e.accelerationIncludingGravity
+      let x = acceleration.x
+      let y = acceleration.y
+      let lastX = 0
+      let lastY = 0
+      if (Math.abs(x - lastX) > speed || Math.abs(y - lastY) > speed) {
+        if (this.value && window.confirm('是否清空输入字符?')) {
+          this.value = ''
+        }
+      }
+    })
+  },
   data () {
     return {
       value: ''
     }
   },
   methods: {
-    clean () {
-      this.value = ''
+    selectAll (e) {
+      e.target.select()
     },
     submit () {
       console.log('submit')
@@ -41,67 +66,94 @@ export default {
 }
 
 .wrapper {
-  width: 80%;
-  margin: 100px auto 0 auto;
+  width: 90%;
+  margin: 0 auto;
   position: relative;;
 }
 
 .logo {
-  width: 200px;
-  height: 200px;
-  margin: 0 auto 100px auto;
+  width: 100px;
+  height: 100px;
+  margin: 20px auto 40px auto;
 
   background-image: url(../images/logo.png);
   background-position: center;
   background-repeat: no-repeat;
+  background-size: contain;
+}
+
+.content {
+  width: 100%;
+  box-sizing: border-box;
+  position: relative;
+  border: 1px solid #ccc;
+
+  &:focus, &:hover {
+    border: 1px solid #5dc596;
+  }
+}
+
+.bar {
+  width: calc(100% - 30px);
+  position: relative;
+}
+
+.input {
+  width: 100%;
+  font-size: 2rem;
+  border: none;
+  outline: none;
+  padding: 10px 50px 10px 10px;
+  position: relative;
+  box-sizing: border-box;
+  color: #666;
 }
 
 .search {
+  font-size: 3rem;
+  display: inline-block;
+  position: absolute;
+  padding: 4px 15px;
+  right: 0px;
+  top: 0px;
+  bottom: 0px;
+  color: white;
+  cursor: pointer;
+  background-color: #5dc596;
+}
+
+.auto {
   width: 100%;
+  padding: 10px;
+  background-color: #eee;
   box-sizing: border-box;
+  font-size: 2rem;
+  border: 1px solid #ccc;
+  border-top: 0;
+}
+@media screen and (min-width:960px) {
+  .wrapper {
+    width: 80%;
+  }
+}
 
-  .input {
-    width: 100%;
-    position: relative;
-
-    .input-bar {
-      width: 100%;
-      font-size: 4rem;
-      border: 1px solid #ccc;
-      outline: none;
-      padding: 20px 90px 20px 20px;
-      position: relative;
-      box-sizing: border-box;
-
-      &:focus, &:hover {
-        border: 1px solid #5dc596;
-      }
-    }
-
-    .input-clean {
-      width: 50px;
-      height: 50px;
-      background: #333;
-
-      position: absolute;
-      right: 20px;
-      top: 20px;
-      z-index: 10;
-    }
+@media screen and (min-width:1024px) {
+  .wrapper {
+    width: 60%;
   }
 
-  .submit {
-    width: 100%;
-    display: inline-block;
-    font-size: 3rem;
-    padding: 20px;
-    text-align: center;
-    border: 1px solid #5dc596;
-    color: white;
-    background-color: #5dc596;
-    margin-top: 40px;
-    letter-spacing: 4px;
-    box-sizing: border-box;
+  .input, .submit {
+    // font-size: 2.4rem;
+  }
+}
+
+@media screen and (min-width:1240px) {
+  .wrapper {
+    width: 50%;
+  }
+
+  .input, .submit {
+    // font-size: 2rem;
   }
 }
 </style>
