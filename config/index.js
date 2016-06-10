65de4a3e7d88618475760555cf88027e765b824d
qianjiahao <1041897388@qq.com>
2016-06-10 21:42:15 +0800

    modify webpack config

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     productionSourceMap: false,
     // gzip off by default as many popular static hosts such as
     // surge or netlify already gzip all static assets for you
